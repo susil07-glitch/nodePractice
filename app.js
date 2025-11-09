e6e44c0a9534cd98b1cef1699855c7b7c20dc239
Sushil Sharma <sushilsharma@Sushils-MacBook-Air.local>
2025-11-09 22:58:58 +0545

    day1

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express')
 const app = express()
+// for the get response and request //
+app.get("/",(req,res)=>{
+    console.log(req)
+    res.status(200).json({
+        message:'this is begning of fullstack mern '
+    })
+})
 
 
 app.listen(3000,()=>{
