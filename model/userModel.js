ed54dcffc9f8a7dd6d6791bb5e77c6d1178ef4b7
Sushil Sharma <sushilsharma@Sushils-MacBook-Air.local>
2025-11-15 22:31:06 +0545

    model

---
+++ model/userModel.js
@@ -0,0 +1,25 @@
+const mongoose= require('mongoose')
+
+const Schema= mongoose.Schema
+
+const blogSchema= new Schema({
+    title:{
+        type:String,
+        unique:true
+    },
+    subtitle:{
+        type:String
+    },
+
+    description:{
+        type:Text
+    },
+
+    image:{
+        type:String
+    }
+})
+
+ const Blog = mongoose.model('Blog',blogSchema)
+
+ module.exports= Blog
