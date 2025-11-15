b18b5ce124ea739ae580170491103d60fb9998c3
Sushil Sharma <sushilsharma@Sushils-MacBook-Air.local>
2025-11-15 23:08:46 +0545

    blog api

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const connectToDatabase=require('./Databse/')
 const app = express()
+app.use(express.json())
 
 connectToDatabase()
 
@@ -17,6 +18,13 @@ app.get("/",(req,res)=>{
 app.get("/about",(req,res)=>{
     res.status(200).json({
         message:'this is about page'
+    })
+})
+
+app.post ("/blog",(req,res)=>{
+    console.log(req.body)
+    res.status(200).json({
+        message:"this api is hitted "
     })
 })
 
